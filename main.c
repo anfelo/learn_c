b7bb0018e3b1e50496e71bee705a678d7006e521
Andres Osorio <af.osorio1341@gmail.com>
2023-04-08 09:55:46 +0200

    Add power function

--- main.c
+++ main.c
@@ -1,31 +1,26 @@
 #include <stdio.h>
 
+int power(int m, int n);
+
 /*
- * Count digits, white space , others
+ * Test power function
  */
 int main()
 {
-    int c, i, nwhite, nother;
-    int ndigit[10];
-
-    nwhite = nother = 0;
-    for (i = 0; i < 10; ++i)
-        ndigit[i] = 0;
-
-    while ((c = getchar()) != EOF) {
-        if (c >= '0' && c <= '9')
-            ++ndigit[c-'0'];
-        else if (c == ' ' || c == '\n' || c == '\t')
-            ++nwhite;
-        else
-            ++nother;
-    }
-
-    printf("digits =");
+    int i;
 
     for (i = 0; i < 10; ++i)
-        printf("%d", ndigit[i]);
-
-    printf(" , white space = %d, other = %d\n", nwhite, nother);
+        printf("%d %d %d\n", i, power(2,i), power(-3, i));
+    return 0;
 }
 
+int power(int base, int n)
+{
+    int i,p;
+
+    p = 1;
+    for (i = 1; i <= n; ++i)
+        p = p * base;
+
+    return p;
+}
