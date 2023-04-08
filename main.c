2bfdb78ced83005631d062f581c77d0097e539ea
Andres Osorio <af.osorio1341@gmail.com>
2023-04-08 08:46:34 +0200

    Add example of how to create an array

--- main.c
+++ main.c
@@ -1,30 +1,31 @@
 #include <stdio.h>
 
-#define IN 1
-#define OUT 0
-
 /*
- * Copy input to output
-*/
+ * Count digits, white space , others
+ */
 int main()
 {
-    int c, nl, nw, nc, state;
+    int c, i, nwhite, nother;
+    int ndigit[10];
 
-    state = OUT;
-    nl = nw = nc = 0;
+    nwhite = nother = 0;
+    for (i = 0; i < 10; ++i)
+        ndigit[i] = 0;
+
     while ((c = getchar()) != EOF) {
-        ++nc;
-        if (c == '\n') {
-            ++nl;
-        }
-
-        if (c == ' ' || c == '\n' || c == '\t') {
-            state = OUT;
-        } else if (state == OUT) {
-            state = IN;
-            ++nw;
-        }
+        if (c >= '0' && c <= '9')
+            ++ndigit[c-'0'];
+        else if (c == ' ' || c == '\n' || c == '\t')
+            ++nwhite;
+        else
+            ++nother;
     }
-    printf("%d%d%d\n", nl, nw, nc);
+
+    printf("digits =");
+
+    for (i = 0; i < 10; ++i)
+        printf("%d", ndigit[i]);
+
+    printf(" , white space = %d, other = %d\n", nwhite, nother);
 }
 
