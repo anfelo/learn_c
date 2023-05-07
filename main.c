4595af157b27a86d287fb911291145d60d7d93b3
Andres Osorio <af.osorio1341@gmail.com>
2023-05-07 07:57:31 +0200

    Add concat files example

--- main.c
+++ main.c
@@ -1,6 +1,42 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main()
+/* cat: concatenate files, version 1 */
+int main(int argc, char *argv[])
 {
-    printf("hello, world\n");
+    FILE *fp;
+    void filecopy(FILE *, FILE *);
+    char *prog = argv[0];
+
+    if (argc == 1)
+        filecopy(stdin, stdout);
+    else
+        while (--argc > 0) {
+            if ((fp = fopen(*++argv, "r")) == NULL)
+            {
+                fprintf(stderr, "%s: can't open %s\n", prog, *argv);
+                exit(1);
+            }
+            else
+            {
+                filecopy(fp, stdout);
+                fclose(fp);
+            }
+        }
+    if (ferror(stdout))
+    {
+        fprintf(stderr, "%s: error writing stdout\n", prog);
+        exit(2);
+    }
+    exit(0);
+}
+
+/* filecopy: copy file ifp to file ofp */
+void filecopy(FILE *ifp, FILE *ofp)
+{
+    int c;
+
+    while ((c = getc(ifp)) != EOF) {
+        putc(c, ofp);
+    }
 }
