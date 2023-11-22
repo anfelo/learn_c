4583b3f5f2ffa68e9647e8bc34c3d6060c572c17
Andres Osorio <af.osorio1341@gmail.com>
2023-11-22 08:35:09 +0000

    add sorting algos

--- main.c
+++ main.c
@@ -1,51 +1,240 @@
+#include <stddef.h>
 #include <stdio.h>
-#include <time.h>
+#include <string.h>
 
-int year_progress();
-void print_progress(int progress);
+void swap_nums(int *a, int *b);
+void print_int_array(int *arr, size_t size);
 
-/*
- * Test power function
- */
-int main()
+void bubblesort(int *arr, size_t size);
+void insertionsort(int *arr, size_t size);
+void selectionsort(int *arr, size_t size);
+
+void quicksort(int *arr, int low, int high);
+int partition(int *arr, int low, int high);
+
+void mergesort(int *arr, int left, int right);
+void merge(int *arr, int left, int mid, int right);
+
+int main(int argc, char *argv[])
 {
-    int progress;
+    if (argc < 2)
+    {
+        printf("No algo specified. Try bubblesort.");
+        return 1;
+    }
 
-    progress = year_progress();
-    print_progress(progress);
+    int my_arr[] = { 35, 29, 8, 90, 15, 33 };
+    size_t arr_len = sizeof(my_arr) / sizeof(my_arr[0]);
+
+    // Initial array
+    print_int_array(my_arr, arr_len);
+
+    if (strcmp(argv[1], "bubblesort") == 0)
+    {
+        bubblesort(my_arr, arr_len);
+    }
+    else if (strcmp(argv[1], "selectionsort") == 0)
+    {
+        selectionsort(my_arr, arr_len);
+    }
+    else if (strcmp(argv[1], "insertionsort") == 0)
+    {
+        insertionsort(my_arr, arr_len);
+    }
+    else if (strcmp(argv[1], "quicksort") == 0)
+    {
+        quicksort(my_arr, 0, (int)arr_len);
+    }
+    else if (strcmp(argv[1], "mergesort") == 0)
+    {
+        mergesort(my_arr, 0, (int)arr_len);
+    }
+    else
+    {
+        printf("No matching sorting algo\n");
+        return -1;
+    }
+
+    // sorted array
+    print_int_array(my_arr, arr_len);
 
     return 0;
 }
 
-int year_progress()
+void bubblesort(int *arr, size_t size)
 {
-    int ndays, nday;
+    int i, j;
+    short swapped = 1;
 
-    time_t t = time(NULL);
-    struct tm *tm = localtime(&t);
-
-    nday = ndays = tm->tm_yday + 1;
-
-    if (tm->tm_year % 4 == 0 && (tm->tm_year % 100 != 0 || tm->tm_year % 400 == 0))
-        ndays = 366;
-    else
-        ndays = 365;
-
-    return (nday * 100) / ndays;
+    while (swapped)
+    {
+        swapped = 0;
+        for (j = 0; j < size - 1; j++)
+        {
+            if (arr[j + 1] < arr[j])
+            {
+                swap_nums(&arr[j], &arr[j + 1]);
+                swapped = 1;
+            }
+        }
+    }
 }
 
-void print_progress(int progress)
+void selectionsort(int *arr, size_t size)
 {
-    int i, progress_bars;
+    int i;
 
-    progress_bars = progress / 10;
+    for (i = 0; i < size; i++)
+    {
+        int max = 0;
+        int j;
 
-    printf("Progress: |");
-    for (i = 0; i < 10; ++i) {
-        if (i <= progress_bars)
-            printf("█");
-        else
-            printf("-");
+        for (j = 0; j < size - i; j++)
+        {
+            if (arr[j] > arr[max])
+            {
+                max = j;
+            }
+        }
+
+        int last = size - 1 - i;
+        if (max != last)
+        {
+            swap_nums(&arr[max], &arr[last]);
+        }
     }
-    printf("| %d%% Complete\n", progress);
+}
+
+void insertionsort(int *arr, size_t size)
+{
+    int i;
+
+    for (i = 1; i < size; i++)
+    {
+        int curr = arr[i];
+        int j = i;
+
+        while (j >= 1 && arr[j - 1] > curr)
+        {
+            arr[j] = arr[j - 1];
+            j--;
+        }
+
+        arr[j] = curr;
+    }
+}
+
+void quicksort(int *arr, int low, int high)
+{
+    if (low < high)
+    {
+        int pivot = partition(arr, low, high);
+        quicksort(arr, low, pivot);
+        quicksort(arr, pivot + 1, high);
+    }
+}
+
+int partition(int *arr, int low, int high)
+{
+    int pivot = arr[low];
+    int left = low;
+    int right = high;
+
+    while (left < right)
+    {
+        while (arr[left] <= pivot && left < high)
+        {
+            left++;
+        }
+
+        while (arr[right] > pivot && right > low)
+        {
+            right--;
+        }
+
+        if (left < right)
+        {
+            swap_nums(&arr[left], &arr[right]);
+        }
+    }
+
+    arr[low] = arr[right];
+    arr[right] = pivot;
+    return right;
+}
+
+void mergesort(int *arr, int left, int right)
+{
+    if (left < right) {
+        int mid = left + (right - left) / 2;
+        mergesort(arr, left, mid);
+        mergesort(arr, mid + 1, right);
+        merge(arr, left, mid, right);
+    }
+}
+
+void merge(int *arr, int left, int mid, int right)
+{
+    int l_size = mid - left + 1;
+    int r_size = right - mid;
+
+    int l_arr[l_size];
+    int r_arr[r_size];
+
+    for (int i = 0; i < l_size; i++) {
+        l_arr[i] = arr[left + i];
+    }
+
+    for (int i = 0; i < r_size; i++) {
+        r_arr[i] = arr[mid + i + 1];
+    }
+
+    int i = 0;
+    int j = 0;
+    int k = left;
+
+    while (i < l_size && j < r_size)
+    {
+        if (l_arr[i] <= r_arr[j])
+        {
+            arr[k] = l_arr[i];
+            i++;
+        }
+        else
+        {
+            arr[k] = r_arr[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < l_size)
+    {
+        arr[k] = l_arr[i];
+        i++;
+        k++;
+    }
+
+    while (j < r_size)
+    {
+        arr[k] = r_arr[j];
+        j++;
+        k++;
+    }
+}
+
+void swap_nums(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void print_int_array(int *arr, size_t size) {
+    int i;
+    printf("[ ");
+    for (i = 0; i < size; i++) {
+        printf(" %d ", arr[i]);
+    }
+    printf(" ]\n");
 }
